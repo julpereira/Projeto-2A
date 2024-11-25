9f0e61acb6d09fc53f6b954932c0bfb2bc47ca33
julpereira <julia.p@estudante.ifro.edu.br>
2024-11-25 10:59:04 -0400

    commit

--- cabecalho/projeto/src/app/inicio/page.js
+++ cabecalho/projeto/src/app/inicio/page.js
@@ -3,7 +3,7 @@ import styles from "../inicio/inicio.module.css";
 export default function Inicio() {
     return(
         <main>
-            <h1 className={styles.inicio}>Inicio</h1>
+            <h1 className={styles.inicio}>Início</h1>
             <img src="/image/imagem1.jpg" alt="Imagem da Home" className={styles.pageImage} />
             <p className={styles.paragrafo}>Imagem gerada por IA</p>
         </main>
--- cabecalho/projeto/src/app/page.js
+++ cabecalho/projeto/src/app/page.js
@@ -3,7 +3,7 @@ import styles from "../app/page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Olá</h1>
+      <h1 className={styles.bemvindo}>Olá</h1>
       <p className={styles.paragrafo}>Seja bem vindo(a)!</p>
     </div>   
   );
